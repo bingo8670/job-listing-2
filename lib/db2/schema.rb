1b0652db9f45e84297d7efa3184da6bc346b7b78
bingo8670 <307286130@qq.com>
2017-04-29 19:50:32 +0800

    step 7 upload resume 11-2done but admin have questions

--- lib/db2/schema.rb
+++ lib/db2/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429113852) do
+ActiveRecord::Schema.define(version: 20170429092912) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20170429113852) do
   create_table "resumes", force: :cascade do |t|
     t.integer  "job_id"
     t.integer  "user_id"
-    t.text     "content"
+    t.text     "conternt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "attachment"
